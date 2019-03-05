594720ac1a26bd9523eca0571f03e04eb581080a
Ishan <shahishan198@gmail.com>
2019-03-05 21:00:44 +0530

    codes added

---
+++ pins.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	int t;
+	cin >> t;
+	while(t--)
+	{
+	    long n;
+        cin >> n;
+		cout<<"1 1";
+		for(int i=0;i<n/2;i++)
+            cout<<'0';
+		cout<<endl;
+	}
+	return 0;
+}
